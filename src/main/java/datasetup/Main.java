45be9679151104ba14125366f61fb91225cb45dc
ilyapesin <pesinilya@gmail.com>
2023-11-01 20:32:30 +0200

    object part2

--- src/main/java/datasetup/Main.java
+++ src/main/java/datasetup/Main.java
@@ -1,7 +1,20 @@
 package datasetup;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        ObjData objData = new ObjData();
 
+        List<DataCommonDto> list = objData.fillData();
+
+        System.out.println(list.get(0).getUserDto().getEmail());
+        System.out.println(list.get(0).getUserDto().getId());
+        System.out.println(list.get(0).getCarDTO()[0].getModel());
+        System.out.println(list.get(0).getCarDTO()[0].getId());
+        System.out.println(list.get(0).getCarDTO()[1].getModel());
+        System.out.println(list.get(0).getCarDTO()[1].getId());
+        System.out.println(list.get(1).getUserDto().getEmail());
+        System.out.println(list.get(1).getUserDto().getId());
     }
 }
--- src/main/java/datasetup/ObjData.java
+++ src/main/java/datasetup/ObjData.java
@@ -12,6 +12,22 @@ public class ObjData {
 
     public List<DataCommonDto> fillData(){
         //list.add
+        UserDto userDto=new UserDto("email@1",1111);
+        UserDto userDto1=new UserDto("email@2",2222);
+        CarDTO carDTO=new CarDTO("model1",111);
+        CarDTO carDTO1=new CarDTO("model2",222);
+        CarDTO[] cars={carDTO, carDTO1};
+        DataCommonDto dataCommonDto=new DataCommonDto(userDto,cars);
+        DataCommonDto dataCommonDto1=new DataCommonDto(userDto1,cars);
+        list.add(dataCommonDto);
+        list.add(dataCommonDto1);
         return list;
+    }
+
+    @Override
+    public String toString() {
+        return "ObjData{" +
+                "list=" + list +
+                '}';
     }
 }
