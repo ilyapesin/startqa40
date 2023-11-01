74923713c9cb3689cf8786c2a5f64cdebcdc16a7
ilyapesin <pesinilya@gmail.com>
2023-11-01 18:11:41 +0200

    ExceptionComit

--- src/main/java/org/example/ExceptionCommon.java
+++ src/main/java/org/example/ExceptionCommon.java
@@ -2,18 +2,47 @@ package org.example;
 
 public class ExceptionCommon {
     public static void main(String[] args) {
-            try {
-
-            }
-            catch (Exception e)
-            {
-
-            }
-            finally
-            {
-
-            }
-
-
+        try {
+        System.out.println(9/0);
+    } catch (NullPointerException e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+    } catch (ArithmeticException e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+    } catch (Exception e) {
+        System.out.println(e.getMessage());
+        System.out.println(e.getStackTrace());
+    } finally {
+        System.out.println("finally");
     }
+
+        try {
+            System.out.println(9/0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        pause();
+        delimetr(5,0);
+    }
+
+    public static void pause() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int delimetr (int i, int b){
+        int res = 0;
+        try {
+            res = i/b;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return res;
+    }
+
 }
