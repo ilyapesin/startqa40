67ee5a9097698a8cd401923d96c0c893387da5f6
ilyapesin <pesinilya@gmail.com>
2023-11-01 22:08:36 +0200

    object part3

--- src/main/java/datasetup/Main.java
+++ src/main/java/datasetup/Main.java
@@ -8,13 +8,20 @@ public class Main {
 
         List<DataCommonDto> list = objData.fillData();
 
-        System.out.println(list.get(0).getUserDto().getEmail());
-        System.out.println(list.get(0).getUserDto().getId());
+        System.out.println("User name: "+list.get(0).getUserDto().getEmail());
+        System.out.println("ID :"+list.get(0).getUserDto().getId());
         System.out.println(list.get(0).getCarDTO()[0].getModel());
         System.out.println(list.get(0).getCarDTO()[0].getId());
         System.out.println(list.get(0).getCarDTO()[1].getModel());
         System.out.println(list.get(0).getCarDTO()[1].getId());
         System.out.println(list.get(1).getUserDto().getEmail());
         System.out.println(list.get(1).getUserDto().getId());
+        System.out.println(list.get(1).getCarDTO()[0].getModel());
+        System.out.println(list.get(1).getCarDTO()[0].getId());
+        System.out.println(list.get(1).getCarDTO()[1].getModel());
+        System.out.println(list.get(1).getCarDTO()[1].getId());
+        System.out.println(list.get(1).getCarDTO()[2].getModel());
+        System.out.println(list.get(1).getCarDTO()[2].getId());
+
     }
 }
--- src/main/java/datasetup/ObjData.java
+++ src/main/java/datasetup/ObjData.java
@@ -14,11 +14,13 @@ public class ObjData {
         //list.add
         UserDto userDto=new UserDto("email@1",1111);
         UserDto userDto1=new UserDto("email@2",2222);
-        CarDTO carDTO=new CarDTO("model1",111);
-        CarDTO carDTO1=new CarDTO("model2",222);
-        CarDTO[] cars={carDTO, carDTO1};
+        CarDTO carDTO1=new CarDTO("model1",111);
+        CarDTO carDTO2=new CarDTO("model2",222);
+        CarDTO carDTO3=new CarDTO("model3",333);
+        CarDTO[] cars={carDTO1, carDTO2};
+        CarDTO[] cars1={carDTO1,carDTO2,carDTO3};
         DataCommonDto dataCommonDto=new DataCommonDto(userDto,cars);
-        DataCommonDto dataCommonDto1=new DataCommonDto(userDto1,cars);
+        DataCommonDto dataCommonDto1=new DataCommonDto(userDto1,cars1);
         list.add(dataCommonDto);
         list.add(dataCommonDto1);
         return list;
